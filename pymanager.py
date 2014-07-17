67851be4fb4b164b73b9f83112d99f34160c8111
Evan Jeffrey <ejeffrey@physics.ucsb.edu>
2014-07-17 16:05:36 -0700

    Move last uncommited changes from SVN repo over to git

--- pymanager.py
+++ pymanager.py
@@ -28,6 +28,18 @@ import manager_config
 import blacklist
 
 class EndianAdaptingProtocol(LabradProtocol):
+    '''
+    This is only for use by the labrad manager.  The labrad protocol
+    allows each client to choose their endianness, and the labrad
+    manager detects that implicitly from the first packet and does
+    conversions as necessary.  
+
+    It would be better (and should be implemented in future versions)
+    that all communication is done in big-endian aka network byte
+    order.  If done properly it doesn't add much overhead to
+    flattening and unflattening, and we save effort of doing multiple
+    conversions in the manager.
+    '''
     def __init__(self):
         LabradProtocol.__init__(self)
         self.endian=None
@@ -52,6 +64,8 @@ class EndianAdaptingProtocol(LabradProtocol):
             self.packetStream.send(data)
     def sendPacket(self, target, context, request, records):
         """Send a raw packet to the specified target."""
+        # FIXME: if transport.write() fails asynchronously, we need an errback
+        # to generate an error response for the source.
         raw = flattenPacket(target, context, request, records, endianness=self.endian)
         self.transport.write(raw)
 
@@ -66,7 +80,8 @@ class LabradManager(EndianAdaptingProtocol):
 
     def connectionMade(self):
         '''
-        Need to check blacklist
+        Handle a new connection.  We register with the manager which 
+        is responsible for checking the blacklist.
         '''
         try:
             self.factory.connectionMade(self)
@@ -80,6 +95,7 @@ class LabradManager(EndianAdaptingProtocol):
             self.pw_challenge = base64.b64encode(twisted.python.randbytes.secureRandom(10))
         except twisted.python.randbytes.SecureRandomNotAvailable:
             self.transport.abortConnection()
+
     def connectionLost(self, reason):
         EndianAdaptingProtocol.connectionLost(self, reason)
         if self.ID:
@@ -98,12 +114,28 @@ class LabradManager(EndianAdaptingProtocol):
     # happens in clients, so the arguments names are reversed from the
     # base class.
     def packetReceived(self, dest, context, request, records):
+        '''
+        Packet received from a client.  If the connection does not yet
+        have an ID, pass to the login code.  Otherwise pass to the
+        factory which does communication with the manager server or
+        other clients.
+        '''
         if not self.ID:
             self.handleLogon(dest, context, request, records)
             return # discard messages before we are connected
         try:
             self.factory.handlePacket(self.ID, dest, context, request, records)
         except Exception as e:
+            # If the manager can't figure out how to dispach the
+            # packet, we need to generate an error response here or
+            # the client will wait forever for a response.  If
+            # handlePacket succeeds, it is required that one way or
+            # another an error or a response is returned to the
+            # client.  In particular. this means that if the eventual
+            # call to write() fails asynchronously, we need to make
+            # sure an errback generates an error response!  This is
+            # not currently done!  FIXME
+
             print "handlePacket error, generating error response"
             response = []
             for record in records:
@@ -116,16 +148,28 @@ class LabradManager(EndianAdaptingProtocol):
             raise
 
     def sendPacket(self, dest, context, request, records):
+        '''
+        Sends a packet from the manager to the client.
+        '''
         #print "Sending packet %s to dest: %s %s records: %s" % (request, dest, context, records)
         EndianAdaptingProtocol.sendPacket(self, dest, context, request, records)
 
     def handleLogon(self, dest, context, request, records):
+        '''
+        It takes 3 packets to set up a connection.  The first one is used to identify the endianness,
+        the second for authentication, and the final tells whether the connection is for a client
+        or server and assigns the connection ID.
+        '''
+        # Packet 1: origin client, target 1, context any, request ID: positive, no records
+        # Response: from source ID 1, same context, -request ID, single response settnig 0: (s) pw challenge
         if self.state==0:   # Connect
             if dest==1 and not len(records):
                 self.state = 1
                 self.sendPacket(dest, context, -request, [(0, self.pw_challenge, 's')]) # send password challenge
             else:
                 self.transport.abortConnection()
+        # Packet 2: origin client: target 1, context any, request ID: positive, setting 0 MD5(pw_challenge + pw)
+        # Response: from setting 0: (s) welcome message or error
         elif self.state==1: # Authentication
             if dest==1 and len(records)==1:
                 setting, data = records[0]
@@ -141,6 +185,9 @@ class LabradManager(EndianAdaptingProtocol):
                     self.transport.loseConnection()
             else:
                 self.transport.abortConnection()
+        # Packet 3: target 1, context any, request ID any, setting 0 (ws) or (wsss) for client/server
+        # containing protocol version, client/server name, description, remarks.
+        # Response: from setting 0: (w) connection ID or error.
         elif self.state==2: # Client/Server Selection
             if dest==1 and len(records) == 1:
                 setting, data = records[0]
@@ -177,11 +224,20 @@ class LabradManager(EndianAdaptingProtocol):
                     self.sendPacket(dest, context, -request, [(0,e)])
 
 def runLocalServer(cfg, srv):
+    '''
+    Connect a server to the manager over the loopback connection.
+    '''
     srv.password = cfg.password
     reactor.connectTCP('localhost', cfg.port, srv)
     
 # ServerFactory 1 per server.  Constructs Protocol objects for every connection
 class LabradManagerFactory(protocol.ServerFactory):
+    '''
+    The LabradManagerFactory actually handles most of the business of the labrad manager
+    since it knows about all the client connections.  This takes care of receiving messages
+    and dispatching them to the correct client protocol object.  Requests targeted at the
+    manager are intercepted and sent to a special in-memory server.
+    '''
     protocol = LabradManager
     def __init__(self, cfg, blacklist):
         # 
@@ -213,8 +269,19 @@ class LabradManagerFactory(protocol.ServerFactory):
         target = self.connections[dest]
 #        print "tgt: %s, src: %s, ctx: %s, req: %s, rec: %s" % (target.ID, source, context, request, records)
         if isinstance(target, InMemoryProtocol):
+            # the only InMemoryProtocol is the Manager itself (the
+            # registry is in the same process but connects via TCP to
+            # the loopback address.  The key difference for an
+            # InMemoryProtocol is that it doesn't have an underlying
+            # transport and the messages are not flattened and
+            # unflattened.
             target.requestReceived(source, context, request, records)
         else:
+            # This compares the record data types to those registered
+            # for each server setting ID, and attempts to coerce them.
+            # This is slow, and needs to be optimized, possibly with a
+            # C implementation.  Better yet, we would get the records
+            # unflattened.
             try:
                 records = self.manager.update_types(source, dest, request, records)
             except Exception as e:
@@ -238,12 +305,22 @@ class LabradManagerFactory(protocol.ServerFactory):
         print "starting factory"
         protocol.ServerFactory.startFactory(self)
     def register_server(self, protocol, server_name, desc, remarks):
+        '''
+        Called when a new server connection happens.  Here we just need to know
+        the ID->Protocol object mapping, but we need to also notify the in-memory
+        manager server so that it can manage the list of settings.
+        '''
         ID = self.manager.register_server(server_name, desc, remarks)
         if ID:
             self.connections[ID] = protocol
         return ID
 
     def register_client(self, protocol, client_name):
+        '''
+        Called when a new client connection happens.  Here we just need to know
+        the ID->Protocol object mapping, but we need to also notify the in-memory
+        manager server.
+        '''
         ID = self.manager.register_client(client_name)
         if ID:
             self.connections[ID] = protocol
@@ -255,6 +332,9 @@ class LabradManagerFactory(protocol.ServerFactory):
         del self.connections[ID]
 
     def connectionMade(self, prot):
+        '''
+        Checks the blacklist.
+        '''
         addr = prot.transport.getPeer()
         if not self.blacklist.check_ip(addr.host):
             raise RuntimeError("IP blacklisted")
@@ -280,6 +360,7 @@ def main():
     listener = reactor.listenTCP(cfg.port, factory)
     reg = registry.Registry(cfg.registry)
     reg.password = cfg.password
+    # Registry is in-process, but connects over a normal socket.
     reactor.connectTCP('localhost', cfg.port, reg)
     reactor.run()
     
